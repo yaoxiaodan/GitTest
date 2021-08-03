82b9f4a18bc2bab345661626bd4f249047d3e0b7
yaoxiaodan <2568173518@qq.com>
2021-08-03 14:47:31 +0800

    新增加项目文件

---
+++ GitTest/ApplicationManagement/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ApplicationManagement
+{
+    public class Class1
+    {
+        /// <summary>
+        /// SayHi
+        /// </summary>
+        /// <returns></returns>
+        public string SayHi() {
+            return "你好！";
+        }
+        
+    }
+}
